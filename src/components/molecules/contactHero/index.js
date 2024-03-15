aced9d82bc36053157ef6730c3d8994e88b39a6c
rahul <rahul.vuppala@gaiansolutions.com>
2024-03-16 01:32:44 +0530

    contact

--- src/components/molecules/contactHero/index.js
+++ src/components/molecules/contactHero/index.js
@@ -5,7 +5,7 @@ const ContactHero = () => {
     <div className={style.main}>
         <div className={style.main2}>
 
-        <div className={style.image}><Image src={"/contact/hero.png"} fill></Image></div>
+        <div className={style.image}><div className={style.imageDiv}></div><Image src={"/contact/hero.png"} fill></Image></div>
         <div className={style.content}>
             <h1 className={style.heading}>GET IN TOUCH</h1>
             <h1 className={style.content2}>Reach out, share a question, or provide feedback on our TDG Furniture Exchange. Fill out the form and we will send you a response as soon as possible.</h1>
