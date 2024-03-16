384c7bf8baa7bb800ae5f84d792a6db539d8ad07
rahul <rahul.vuppala@gaiansolutions.com>
2024-03-17 02:19:41 +0530

    chnages

--- src/components/molecules/ImageAndDescription/index.js
+++ src/components/molecules/ImageAndDescription/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const ImageAndDesc = ({img,height,width,content}) => {
     return ( 
     <div className={style.main}>
-        <div className={style.image} style={{height:height,width:width}}><Image src={img} fill ></Image></div>
+        <div className={style.image} style={{height:height}}><Image src={img} fill ></Image></div>
         <h1 className={style.content}>{content}</h1>
     </div> 
     );
