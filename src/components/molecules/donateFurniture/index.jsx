2ca1bd57fa850fcab5370b57d0365e648d3fc852
rahul <rahul.vuppala@gaiansolutions.com>
2024-03-18 12:21:13 +0530

    landing animations

--- src/components/molecules/donateFurniture/index.jsx
+++ src/components/molecules/donateFurniture/index.jsx
@@ -1,9 +1,12 @@
 import style from "./index.module.css"
 import Image from "next/image";
+import AOSComponent from "../AOS";
 const DonateFurniture = ({leftHeading,leftCont,leftImg,rightCont,rightHeading,rightImg}) => {
     return ( 
+        <AOSComponent>
+
         <div className={style.main}>
-            <div className={style.left}>
+            <div data-aos="fade-right" data-aos-duration="400" className={style.left}>
                 <h1 className={style.heading}>{leftHeading}</h1>
                 <h1 className={style.desc}>{leftCont}</h1>
                 <div className={style.line}></div>
@@ -15,7 +18,7 @@ const DonateFurniture = ({leftHeading,leftCont,leftImg,rightCont,rightHeading,ri
                     </div>
                 </div>
             </div>
-            <div className={style.right}>
+            <div data-aos="fade-left" data-aos-duration="400" className={style.right}>
                 <div className={style.image}>
                     <Image src={rightImg} fill></Image>
                     <div className={style.divdiv}>
@@ -26,6 +29,7 @@ const DonateFurniture = ({leftHeading,leftCont,leftImg,rightCont,rightHeading,ri
                 <h1 className={style.desc}>{rightCont}</h1>
             </div>
         </div>
+        </AOSComponent>
      );
 }
  
