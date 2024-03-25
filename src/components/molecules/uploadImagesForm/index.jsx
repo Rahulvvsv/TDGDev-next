a32af143e2097fe05cb9980f02a8c8a3933d485e
rahul <rahul.vuppala@gaiansolutions.com>
2024-03-25 14:21:44 +0530

    changes

--- src/components/molecules/uploadImagesForm/index.jsx
+++ src/components/molecules/uploadImagesForm/index.jsx
@@ -52,7 +52,7 @@
 
 //   return (
 //     <div>
-        
+
 //       {!submitted ? (
 //         <div
 //           initial={{ opacity: 0 }}
@@ -152,207 +152,249 @@ import Image from "next/image";
 import { useState } from "react";
 import { useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import ReCAPTCHA from "react-google-recaptcha";
 import { upLoadData } from "../../../lib/firebase";
+import { useRef } from "react";
+import toast from "react-hot-toast";
 
 const UploadForm = () => {
-    const [data, setData] = useState({});
-    const [file, setFile] = useState(null);
-    const [submitted, setSubmitted] = useState(false);
-    const [previewUrl, setPreview] = useState(null);
-    const [imageUploaded, setImageUploaded] = useState(false);
-    const [errors, setErrors] = useState({});
+  const [data, setData] = useState({});
+  let captcha = useRef(null);
+  const [gtoken,setGtoken] = useState(null)
+  const [file, setFile] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+  const [previewUrl, setPreview] = useState(null);
+  const [imageUploaded, setImageUploaded] = useState(false);
 
-    useEffect(() => {
-        if (!file) {
-            setPreview(undefined);
-            return;
-        }
+  const [errors, setErrors] = useState({});
 
-        const objectUrl = URL.createObjectURL(file);
-        setPreview(objectUrl);
+  useEffect(() => {
+    if (!file) {
+      setPreview(undefined);
+      return;
+    }
 
-        // Free memory when this component is unmounted
-        return () => URL.revokeObjectURL(objectUrl);
-    }, [file]);
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
 
-    const dataSetter = (e) => {
-        const { name, value } = e.target;
-        setData((prev) => ({ ...prev, [name]: value }));
-    };
+    // Free memory when this component is unmounted
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [file]);
 
-    const handleImageChange = (e) => {
-        const imageFile = e.target.files;
-        setFile(imageFile[0]);
-        setData((prevData) => ({
-            ...prevData,
-            image: imageFile,
-        }));
-        setImageUploaded(true);
-    };
+  const dataSetter = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (validateForm()) {
-            upLoadData(data);
-            setSubmitted(true);
-        }
-    };
+  const handleImageChange = (e) => {
+    const imageFile = e.target.files;
+    setFile(imageFile[0]);
+    setData((prevData) => ({
+      ...prevData,
+      image: imageFile,
+    }));
+    setImageUploaded(true);
+  };
 
-    const validateForm = () => {
-        let isValid = true;
-        const newErrors = {};
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+    const google_recaptcha = await fetch("/api/uploadImage", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        gRecaptchaToken: gtoken,
+      }),
+    });
 
-        // Validate full name
-        if (!data.fullName || data.fullName.trim() === "") {
-            newErrors.fullName = "Full name is required";
-            isValid = false;
-        }
+    let data = await google_recaptcha.json();
+    console.log(data);
 
-        if (!data.image) {
-            newErrors.image = "Image is required";
-            isValid = false;
-        }
-        // Validate email
-        if (!data.email || data.email.trim() === "") {
-            newErrors.email = "Email is required";
-            isValid = false;
-        } else if (!/\S+@\S+\.\S+/.test(data.email)) {
-            newErrors.email = "Invalid email address";
-            isValid = false;
-        }
+    if (validateForm()) {
+      upLoadData(data);
+      setSubmitted(true);
+    } else {
+      toast.error("captcha invalid form not submitted");
+    }
+  };
 
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = {};
 
-        // Validate phone
-        if (!data.phone || data.phone.trim() === "") {
-            newErrors.phone = "Phone number is required";
-            isValid = false;
-        }
+    // Validate full name
+    if (!data.fullName || data.fullName.trim() === "") {
+      newErrors.fullName = "Full name is required";
+      isValid = false;
+    }
 
-        // Validate location
-        if (!data.location || data.location.trim() === "") {
-            newErrors.location = "Location is required";
-            isValid = false;
-        }
+    if (!data.image) {
+      newErrors.image = "Image is required";
+      isValid = false;
+    }
+    // Validate email
+    if (!data.email || data.email.trim() === "") {
+      newErrors.email = "Email is required";
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+      newErrors.email = "Invalid email address";
+      isValid = false;
+    }
 
-        // Validate exact location
-        if (!data.exactLocation || data.exactLocation.trim() === "") {
-            newErrors.exactLocation = "Exact location is required";
-            isValid = false;
-        }
+    // Validate phone
+    if (!data.phone || data.phone.trim() === "") {
+      newErrors.phone = "Phone number is required";
+      isValid = false;
+    }
 
-        // Validate type of furniture
-        if (!data.tof || data.tof.trim() === "") {
-            newErrors.tof = "Type of furniture is required";
-            isValid = false;
-        }
+    // Validate location
+    if (!data.location || data.location.trim() === "") {
+      newErrors.location = "Location is required";
+      isValid = false;
+    }
 
-        // Validate description
-        if (!data.desc || data.desc.trim() === "") {
-            newErrors.desc = "Description is required";
-            isValid = false;
-        }
+    // Validate exact location
+    if (!data.exactLocation || data.exactLocation.trim() === "") {
+      newErrors.exactLocation = "Exact location is required";
+      isValid = false;
+    }
 
-        setErrors(newErrors);
-        return isValid;
-    };
+    // Validate type of furniture
+    if (!data.tof || data.tof.trim() === "") {
+      newErrors.tof = "Type of furniture is required";
+      isValid = false;
+    }
 
-    return (
-        <div>
-            {!submitted ? (
-                <div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    className={style.main}
-                >
-                    <div className={style.left}>
-                        <div className={style.imageUpload}>
-                            {file && <Image src={previewUrl} className={style.imageInside} fill />}
-                            <label htmlFor="files" className={style.inputs123}>
-                                <Image src={"/Icon/plus.png"} width={78} height={78} />
-                            </label>
-                            <input
-                                name="image"
-                                onChange={handleImageChange}
-                                id="files"
-                                style={{ visibility: "hidden" }}
-                                accept="image/*"
-                                type="file"
-                            />
-                            <h1 className={style.heading}>UPLOAD IMAGE</h1>
-                        </div>
-                        <br />
-                        {imageUploaded && <h1 className={style.heading}>Image is uploaded</h1>}
-                        {errors.image && <div className={style.error}>{errors.image}</div>}
-                    </div>
+    // Validate description
+    if (!data.desc || data.desc.trim() === "") {
+      newErrors.desc = "Description is required";
+      isValid = false;
+    }
 
-                    <motion.div className={style.right}>
-                        <InputField
-                            onChange={dataSetter}
-                            placeholder={"FULL NAME*"}
-                            name={"fullName"}
-                        />
-                        {errors.fullName && <div className={style.error}>{errors.fullName}</div>}
-                        <InputField
-                            onChange={dataSetter}
-                            placeholder={"EMAIL*"}
-                            name={"email"}
-                        />
-                        {errors.email && <div className={style.error}>{errors.email}</div>}
-                        <InputField
-                            onChange={dataSetter}
-                            placeholder={"PHONE*"}
-                            name={"phone"}
-                        />
-                        {errors.phone && <div className={style.error}>{errors.phone}</div>}
-                        <InputField
-                            placeholder={"LOCATION*"}
-                            name={"location"}
-                            onChange={dataSetter}
-                            type="dropdown"
-                        />
-                        {errors.location && <div className={style.error}>{errors.location}</div>}
-                        <InputField
-                            onChange={dataSetter}
-                            placeholder={"EXACT LOCATION*"}
-                            name={"exactLocation"}
-                        />
-                        {errors.exactLocation && <div className={style.error}>{errors.exactLocation}</div>}
-                        <InputField
-                            onChange={dataSetter}
-                            placeholder={"TYPE OF FURNITURE*"}
-                            name={"tof"}
-                        />
-                        {errors.tof && <div className={style.error}>{errors.tof}</div>}
-                        <InputField
-                            onChange={dataSetter}
-                            placeholder={"DESCRIBE YOUR FURNITURE*"}
-                            name={"desc"}
-                            type="text-area"
-                        />
-                        {errors.desc && <div className={style.error}>{errors.desc}</div>}
-                        <Button
-                            onClick={handleSubmit}
-                            placeholder={"Submit"}
-                            content={"Submit"}
-                            href={""}
-                            fontColor={"white"}
-                        />
-                    </motion.div>
-                </div>
-            ) : (
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    className={style.main2}
-                >
-                    <Image src={"/Icon/4.png"} width={190} height={190} />
-                    <h1 className={style.heading1}>Thank you for your generous offer to donate your Furniture. We will notify you as soon as an opportunity comes up, and you can confirm your intent to donate. We appreciate your support!</h1>
-                </motion.div>
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  return (
+    <div>
+      {!submitted ? (
+        <div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className={style.main}
+        >
+          <div className={style.left}>
+            <div className={style.imageUpload}>
+              {file && (
+                <Image src={previewUrl} className={style.imageInside} fill />
+              )}
+              <label htmlFor="files" className={style.inputs123}>
+                <Image src={"/Icon/plus.png"} width={78} height={78} />
+              </label>
+              <input
+                name="image"
+                onChange={handleImageChange}
+                id="files"
+                style={{ visibility: "hidden" }}
+                accept="image/*"
+                type="file"
+              />
+              <h1 className={style.heading}>UPLOAD IMAGE</h1>
+            </div>
+            <br />
+            {imageUploaded && (
+              <h1 className={style.heading}>Image is uploaded</h1>
             )}
+            {errors.image && <div className={style.error}>{errors.image}</div>}
+          </div>
+
+          <motion.div className={style.right}>
+            <InputField
+              onChange={dataSetter}
+              placeholder={"FULL NAME*"}
+              name={"fullName"}
+            />
+            {errors.fullName && (
+              <div className={style.error}>{errors.fullName}</div>
+            )}
+            <InputField
+              onChange={dataSetter}
+              placeholder={"EMAIL*"}
+              name={"email"}
+            />
+            {errors.email && <div className={style.error}>{errors.email}</div>}
+            <InputField
+              onChange={dataSetter}
+              placeholder={"PHONE*"}
+              name={"phone"}
+            />
+            {errors.phone && <div className={style.error}>{errors.phone}</div>}
+            <InputField
+              placeholder={"LOCATION*"}
+              name={"location"}
+              onChange={dataSetter}
+              type="dropdown"
+            />
+            {errors.location && (
+              <div className={style.error}>{errors.location}</div>
+            )}
+            <InputField
+              onChange={dataSetter}
+              placeholder={"EXACT LOCATION*"}
+              name={"exactLocation"}
+            />
+            {errors.exactLocation && (
+              <div className={style.error}>{errors.exactLocation}</div>
+            )}
+            <InputField
+              onChange={dataSetter}
+              placeholder={"TYPE OF FURNITURE*"}
+              name={"tof"}
+            />
+            {errors.tof && <div className={style.error}>{errors.tof}</div>}
+            <InputField
+              onChange={dataSetter}
+              placeholder={"DESCRIBE YOUR FURNITURE*"}
+              name={"desc"}
+              type="text-area"
+            />
+            {errors.desc && <div className={style.error}>{errors.desc}</div>}
+            <ReCAPTCHA
+              ref={captcha}
+              sitekey={process.env.NEXT_PUBLIC_GOOGLE_SITE_KEY}
+              onChange={(val) => {
+              setGtoken(val);
+              }}
+            ></ReCAPTCHA>
+            <Button
+              onClick={handleSubmit}
+              placeholder={"Submit"}
+              content={"Submit"}
+              href={""}
+              fontColor={"white"}
+            />
+          </motion.div>
         </div>
-    );
+      ) : (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className={style.main2}
+        >
+          <Image src={"/Icon/4.png"} width={190} height={190} />
+          <h1 className={style.heading1}>
+            Thank you for your generous offer to donate your Furniture. We will
+            notify you as soon as an opportunity comes up, and you can confirm
+            your intent to donate. We appreciate your support!
+          </h1>
+        </motion.div>
+      )}
+    </div>
+  );
 };
 
 export default UploadForm;
---
+++ src/pages/api/uploadImage.js
@@ -0,0 +1,46 @@
+
+import { NextResponse } from 'next/server'
+const handler = (req, res) => {
+  if (req.method === "POST") {
+    // console.log(req)
+    try {
+      fetch("https://www.google.com/recaptcha/api/siteverify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `secret=${process.env.SECRET_KEY_GOOGLE}&response=${req.body.gRecaptchaToken}`,
+      })
+        .then((reCaptchaRes) => reCaptchaRes.json())
+        .then((reCaptchaRes) => {
+          console.log(
+            reCaptchaRes,
+            reCaptchaRes.score,
+            "Response from Google reCatpcha verification API"
+          );
+          if (reCaptchaRes.success) {
+            // Save data to the database from here
+            res.status(200).json({
+              status: "success",
+              message: "Enquiry submitted successfully",
+            });
+          } else {
+            res.status(200).json({
+              status: "failure",
+              message: "Google ReCaptcha Failure",
+            });
+          }
+        });
+    } catch (err) {
+      res.status(405).json({
+        status: "failure",
+        message: "Error submitting the enquiry form",
+      });
+    }
+  } else {
+    res.status(405);
+    res.end();
+  }
+};
+
+export default handler;
