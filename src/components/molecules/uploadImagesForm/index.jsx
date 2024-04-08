5853ca5f847c6ebd8a75b07ed4d2d26d51966490
Rahul Vuppala <55148380+Rahulvvsv@users.noreply.github.com>
2024-04-08 23:19:39 +0530

    changes

--- src/components/molecules/uploadImagesForm/index.jsx
+++ src/components/molecules/uploadImagesForm/index.jsx
@@ -181,7 +181,7 @@ const UploadForm = () => {
   }, [file]);
 
   const dataSetter = (e) => {
-    console.log(data);
+    // console.log(data);
     const { name, value } = e.target;
     setData((prev) => ({ ...prev, [name]: value }));
   };
