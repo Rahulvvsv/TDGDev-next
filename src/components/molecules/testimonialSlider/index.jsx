d535f9e16ab33961e6a7fd1541538c43479cb329
Rahul Vuppala <55148380+Rahulvvsv@users.noreply.github.com>
2024-04-16 20:06:05 +0530

    testimonial names

--- src/components/molecules/testimonialSlider/index.jsx
+++ src/components/molecules/testimonialSlider/index.jsx
@@ -72,23 +72,23 @@ export default function App() {
           })
         } */}
             <SwiperSlide>
-        <SingleTestimonial index={0}  testimonial={"I reached out to furniture exchange as soon as my friend’s child was diagnosed with cancer. The girl needed a new bed, and I thought TDG Furniture exchange might have something that could work. Since she has to spend a lot of time in bed, a nice set would definitely help improve her mood. The communication and seamless delivery of a beautiful bedroom set were unbelievable! The family was so moved by the donation."}  name={""} surname={"  "} ></SingleTestimonial>
+        <SingleTestimonial index={0}  testimonial={"TDG Furniture Exchange provided a beautiful bedroom set for a child battling cancer, uplifting her spirits immensely. Grateful for their incredible work!"}  name={""} surname={"  "} ></SingleTestimonial>
         </SwiperSlide>
             <SwiperSlide>
-        <SingleTestimonial index={0}  testimonial={"I have no words for the incredible work you do and did. I am very impressed with the unbelievable experience you had made possible for both parties! Looking forward to helping many more people through Furniture Exchange!"}  name={""} surname={""} ></SingleTestimonial>
+        <SingleTestimonial index={0}  testimonial={"As a single parent, TDG Furniture Exchange restored my faith in humanity with quality furniture donations. A beacon of hope in our community"}  name={""} surname={""} ></SingleTestimonial>
         </SwiperSlide>
             <SwiperSlide>
-        <SingleTestimonial index={0}  testimonial={"TDG Furniture Exchange truly made a difference in my life. As a single parent struggling to furnish our new home, I stumbled upon this amazing platform. Not only did I find quality furniture for my family, but the generosity and support from the donors restored my faith in humanity. Thank you for being a beacon of hope in our community."}  name={""} surname={""} ></SingleTestimonial>
+        <SingleTestimonial index={0}  testimonial={"TDG Furniture Exchange transformed my apartment into a home with essential pieces. Grateful for their kindness and seamless experience!."}  name={""} surname={""} ></SingleTestimonial>
         </SwiperSlide>
             <SwiperSlide>
-        <SingleTestimonial index={0}  testimonial={"I couldn't believe the kindness I encountered through TDG Furniture Exchange. After a recent move, I had to start from scratch with furnishing my apartment. Thanks to this platform, I received not only essential pieces but also beautiful items that transformed my space into a home. The simplicity of connecting with donors made the experience seamless. I'm forever grateful!"}  name={""} surname={""} ></SingleTestimonial>
+        <SingleTestimonial index={0}  testimonial={"Thanks to TDG Furniture Exchange, I furnished my first apartment on a tight budget. Essential pieces in great condition made it possible!"}  name={""} surname={""} ></SingleTestimonial>
         </SwiperSlide>
             <SwiperSlide>
-        <SingleTestimonial index={0}  testimonial={"As a college student moving into my first apartment, I was on a tight budget and didn't have much to spare for furniture. Through this platform, I received essential pieces that were in great condition, allowing me to create a comfortable living space without breaking the bank. Thank you for providing such a valuable service!"}  name={""} surname={""} ></SingleTestimonial>
+        <SingleTestimonial index={0}  testimonial={"Grateful for TDG Furniture Exchange's smart initiative and the armchair they found for our new home. Thank you for your investment!"}  name={""} surname={""} ></SingleTestimonial>
         </SwiperSlide>
-            <SwiperSlide>
+            {/* <SwiperSlide>
         <SingleTestimonial index={0}  testimonial={"Thank you for investing so much time, effort and energy into your amazing furniture initiative! Such a smart initiative, really appreciate the armchair that you found us for our new home. We’re grateful for you!"}  name={""} surname={""} ></SingleTestimonial>
-        </SwiperSlide>
+        </SwiperSlide> */}
             {/* <SwiperSlide>
         <SingleTestimonial index={0}  testimonial={"Furniture exchange was smooth and perfect and very nice wow fantastic delivery service loved the process furniture also was nice not damaged and fast delivery"}  name={""} surname={""} ></SingleTestimonial>
         </SwiperSlide>
