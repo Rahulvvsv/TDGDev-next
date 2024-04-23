c84be0fde45ef0a9bac505a60e3fff35384d5dc8
rahul <rahul.vuppala@gaiansolutions.com>
2024-04-23 16:15:23 +0530

    changes to landing

--- src/components/molecules/landing-secondHeading/index.js
+++ src/components/molecules/landing-secondHeading/index.js
@@ -1,13 +1,14 @@
 import style from "./index.module.css"
 import AOSComponent from "../AOS"
 import Button from "../../atoms/button"
-const SecondHeading = ({heading,contactHeading}) =>{
+const SecondHeading = ({heading,contactHeading,buttonTrue=true}) =>{
     return (
         <AOSComponent>
 
         <div className={style.main} data-aos="fade-down">
             <h1 className={style.fonts}>{heading}</h1>
-            <Button href={"/contactUs"} content={contactHeading} effect={true}></Button>
+           {buttonTrue && <Button href={"/contactUs"} content={contactHeading} effect={true}></Button>
+           }
         </div>
         </AOSComponent>
     )
--- src/components/templates/mainLanding/index.jsx
+++ src/components/templates/mainLanding/index.jsx
@@ -1,31 +1,44 @@
-
-import style from "./index.module.css"
+import style from "./index.module.css";
 import LandingHero from "../../molecules/landing-hero";
 import IconGroupDesc from "../../molecules/icon-group-desc";
 import ThoughtAndLink from "../../molecules/thoughtAndLinks";
 import SecondHeading from "../../molecules/landing-secondHeading";
 import AOSComponent from "../../molecules/AOS";
+import Button from "../../atoms/button";
 export default function Landing() {
   return (
     <section className={style.main1}>
-        <section >
-            <LandingHero></LandingHero>
-        </section>
-        <section className={style.main3}>
-          <SecondHeading heading={"Our Furniture"} contactHeading={"Contact Us"}></SecondHeading>
-        </section>
-        <section className={style.main4}>
-          <IconGroupDesc></IconGroupDesc>
-        </section>
-        <AOSComponent>
+      <section>
+        <LandingHero></LandingHero>
+      </section>
+      <section className={style.main3}>
+        <SecondHeading
+          buttonTrue={false}
+          heading={"Our Process"}
+          contactHeading={"Contact Us"}
+        ></SecondHeading>
+      </section>
+      <section className={style.main4}>
+        <IconGroupDesc></IconGroupDesc>
+      </section>
+      <section className={style.buttonComponent}>
 
+      <Button
+        href={"/contactUs"}
+        content={"Contact Us"}
+        effect={true}
+        ></Button>
+        </section>
+      <AOSComponent>
         <section className={style.main5} data-aos="fade-up">
-          <ThoughtAndLink Description={""}
-          name={""} ButtonContent={"About Us"} href="/aboutUs"
+          <ThoughtAndLink
+            Description={""}
+            name={""}
+            ButtonContent={"About Us"}
+            href="/aboutUs"
           ></ThoughtAndLink>
         </section>
-        
-          </AOSComponent>
+      </AOSComponent>
     </section>
   );
 }
