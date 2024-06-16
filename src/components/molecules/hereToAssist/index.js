a697ca5d7785c9d72b6ec813448d25b5d30d6e55
Rahul Vuppala <55148380+Rahulvvsv@users.noreply.github.com>
2024-06-16 17:14:11 +0530

    contact us changes

--- src/components/molecules/hereToAssist/index.js
+++ src/components/molecules/hereToAssist/index.js
@@ -1,39 +1,64 @@
-import style from "./index.module.css"
+import style from "./index.module.css";
 import Button from "../../atoms/button";
-const HereToAssist = () => {
-    function lwr(str) {
+const HereToAssist = ({ openModal, setEmail }) => {
+  function lwr(str) {
     // Remove spaces from the input string
-    let str2 = str.replace(/\s/g, '');
+    let str2 = str.replace(/\s/g, "");
     // Convert the string to lowercase
     var lowerStr = str2.toLowerCase();
-    let mail =""
-    if(lowerStr == "newyork"){
-        mail = "info"
+    let mail = "";
+    if (lowerStr == "newyork") {
+      mail = "info";
+    } else if (lowerStr == "losangeles") {
+      mail = "la";
+    } else {
+      mail = lowerStr;
     }
-    else if(lowerStr == "losangeles"){
-        mail="la"
-    }
-    else{
-        mail= lowerStr
-    }
-    return {lowerStr,mail};
-}
+    return { lowerStr, mail };
+  }
 
-    let StatesList = ["Baltimore","Cleveland","Jerusalem","Lakewood","Los Angeles","Miami","New Jersey","New York","Toronto"]
-    return ( 
-        <div className={style.main}>
-            <div className={style.main2}>
-
-                <h1 className={style.heading}>We&apos;re here to assist you!</h1>
-                <p className={style.content}>Whether you have questions about our furniture listings, want to schedule a pickup or delivery, or simply want to chat about all things furniture, we&apos;re here to assist you. </p>
-                <div className={style.states + " " + style["grid-container"]}>
-
-                {StatesList.map( (e,key)=><Button effect={false} key={key} content={e} backgroundColor={"white"} width={"90%"} btnClass={2}  className={style["grid-item"]} borderthick={"2px solid rgba(121, 117, 114, 1)"} fontColor={"rgba(121, 117, 114, 1)"} href={"mailto:"+lwr(e).mail+"@tdgfurnitureexchange.com"} ></Button>)}
-                </div>
-                
-            </div>
+  let StatesList = [
+    "Baltimore",
+    "Cleveland",
+    "Jerusalem",
+    "Lakewood",
+    "Los Angeles",
+    "Miami",
+    "New Jersey",
+    "New York",
+    "Toronto",
+  ];
+  return (
+    <div className={style.main}>
+      <div className={style.main2}>
+        <h1 className={style.heading}>We&apos;re here to assist you!</h1>
+        <p className={style.content}>
+          Whether you have questions about our furniture listings, want to
+          schedule a pickup or delivery, or simply want to chat about all things
+          furniture, we&apos;re here to assist you.{" "}
+        </p>
+        <div className={style.states + " " + style["grid-container"]}>
+          {StatesList.map((e, key) => (
+            <Button
+              onClick={() => {
+                console.log("inside this buttton");
+                openModal(true);
+                setEmail(lwr(e).mail + "@tdgfurnitureexchange.com");
+              }}
+              key={key}
+              content={e}
+              backgroundColor={"white"}
+              width={"90%"}
+              btnClass={2}
+              className={style["grid-item"]}
+              borderthick={"2px solid rgba(121, 117, 114, 1)"}
+              fontColor={"rgba(121, 117, 114, 1)"}
+            ></Button>
+          ))}
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default HereToAssist;
--- src/components/templates/contactUs/index.jsx
+++ src/components/templates/contactUs/index.jsx
@@ -1,27 +1,143 @@
-import style from "./index.module.css"
+// import style from "./index.module.css";
+// import ContactHero from "../../molecules/contactHero";
+// import ContactForm from "../../molecules/contactForm";
+// import HereToAssist from "../../molecules/hereToAssist";
+// const ContactUS = () => {
+//   return (
+//     <section>
+//       <section className={style.main3}>
+//         <HereToAssist></HereToAssist>
+//         <Modal
+//           isOpen={modalIsOpen}
+//           onAfterOpen={afterOpenModal}
+//           onRequestClose={closeModal}
+//           className={style.main8}
+//           overlayClassName={style.main10}
+//           subtitle="popup"
+//         >
+//           <section className={style.main9}>
+//             <ContactDonarPopUP
+//               data={data}
+//               setData={setData}
+//               modalCloser={closeModal}
+//             ></ContactDonarPopUP>
+//           </section>
+//         </Modal>
+//       </section>
+//       <section className={style.main1}>
+//         <ContactHero></ContactHero>
+//       </section>
+//       <section className={style.main2}>
+//         <ContactForm></ContactForm>
+//       </section>
+//       <section className={style.main4}>
+//         <h1 className={style.heading}>
+//           TDG Furniture exchange currently operates in Baltimore, Cleveland,
+//           Jerusalem, Lakewood, Los Angeles, Miami, New Jersey, New York and
+//           Toronto.{" "}
+//         </h1>
+//         <br />
+//         <h1 className={style.heading}>
+//           The Designers Group can facilitate and fund the start of a branch in
+//           your neighborhood. Please reach out to info@tdgfurnitureexchange.com
+//           to get involved.
+//         </h1>
+//       </section>
+//     </section>
+//   );
+// };
+
+// export default ContactUS;
+import React, { useState } from "react";
+import style from "./index.module.css";
 import ContactHero from "../../molecules/contactHero";
 import ContactForm from "../../molecules/contactForm";
 import HereToAssist from "../../molecules/hereToAssist";
+import Modal from "react-modal";
+import Button from "@/components/atoms/button";
+import Image from "next/image";
 const ContactUS = () => {
-    return (  
-        <section>
-            <section className={style.main3}>
-                <HereToAssist></HereToAssist>
-            </section>
-            <section className={style.main1}>
-                <ContactHero></ContactHero>
-            </section>
-            <section className={style.main2}>
-                <ContactForm></ContactForm>
-            </section>
-            <section className={style.main4}>
-                <h1 className={style.heading}>TDG Furniture exchange currently operates in Baltimore, Cleveland, Jerusalem, Lakewood, Los Angeles, Miami, New Jersey, New York and Toronto. </h1>
-                <br />
-                <h1 className={style.heading}>The Designers Group can facilitate and fund the start of a branch in your neighborhood. Please reach out to info@tdgfurnitureexchange.com to get involved.</h1>
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState({});
 
-            </section>
-        </section>
-    );
-}
- 
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const afterOpenModal = () => {
+    // Handle after modal is opened
+  };
+
+  return (
+    <section>
+      <section className={style.main3}>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          className={style.main8}
+          overlayClassName={style.main10}
+          subtitle="popup"
+        >
+          <section className={style.main9}>
+            <div
+              onClick={() => {
+                closeModal();
+              }}
+              className={style.closeButton}
+            >
+              <Image
+                src={"/Icon/cross.png"}
+                width={20}
+                height={20}
+                className={style.image}
+              />
+            </div>
+            <Button
+              key={email}
+              content={email}
+              backgroundColor={"white"}
+              width={"90%"}
+              effect={false}
+              btnClass={2}
+              className={style["grid-item"]}
+              borderthick={"2px solid rgba(121, 117, 114, 1)"}
+              fontColor={"rgba(121, 117, 114, 1)"}
+              href={`mailto:${email}`}
+            ></Button>
+          </section>
+        </Modal>
+        <HereToAssist
+          openModal={setModalIsOpen}
+          setEmail={setEmail}
+        ></HereToAssist>
+      </section>
+      <section className={style.main1}>
+        <ContactHero></ContactHero>
+      </section>
+      <section className={style.main2}>
+        <ContactForm></ContactForm>
+      </section>
+      <section className={style.main4}>
+        <h1 className={style.heading}>
+          TDG Furniture exchange currently operates in Baltimore, Cleveland,
+          Jerusalem, Lakewood, Los Angeles, Miami, New Jersey, New York and
+          Toronto.{" "}
+        </h1>
+        <br />
+        <h1 className={style.heading}>
+          The Designers Group can facilitate and fund the start of a branch in
+          your neighborhood. Please reach out to info@tdgfurnitureexchange.com
+          to get involved.
+        </h1>
+      </section>
+    </section>
+  );
+};
+
 export default ContactUS;
