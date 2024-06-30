d6e4f4a5bafeff779c605d34222b75bc93e62699
rahul <rahulwork120@gmail.com>
2024-06-30 10:18:28 +0530

    added SEO tags

--- src/pages/donate/donateFurniture/index.jsx
+++ src/pages/donate/donateFurniture/index.jsx
@@ -1,6 +1,22 @@
 import DonateFurniture from "@/components/templates/donateFurniture";
+import Head from "next/head";
 const index = () => {
-    return (  <DonateFurniture></DonateFurniture>);
-}
- 
+  return (
+    <>
+      <Head>
+        <title>Donate Furniture</title>
+        <meta
+          name="description"
+          content="Donate your extra furniture to TDG Furniture Exchange. Help people to find the right fit for their furniture needs by your generous donation."
+        />
+        <meta
+          name="keywords"
+          content="Donate furniture, TDG Furniture Exchange donation, furniture charity"
+        />
+      </Head>
+      <DonateFurniture></DonateFurniture>
+    </>
+  );
+};
+
 export default index;
